12e1ffedbe80f8f58c7aac65fb5d0891ee4e6987
MakSharonov <mak.sharonov@gmail.com>
2016-02-18 00:28:30 +1000

    Added lessons 9-12

---
+++ p0101listener/src/main/java/com/vl/mak/listener/MainActivity.java
@@ -0,0 +1,40 @@
+package com.vl.mak.listener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btnOk, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut       = (TextView) findViewById(R.id.tvOut);
+        btnOk       = (Button) findViewById(R.id.btnOk);
+        btnCancel   = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.btnOk:
+                        tvOut.setText("okes");
+                        break;
+                    case R.id.btnCancel:
+                        tvOut.setText("cancellito");
+                        break;
+                }
+            }
+        };
+
+        btnOk.setOnClickListener(oclBtn);
+        btnCancel.setOnClickListener(oclBtn);
+    }
+}
---
+++ p0111resvalues/src/main/java/com/vl/mak/resvalues/MainActivity.java
@@ -0,0 +1,25 @@
+package com.vl.mak.resvalues;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+
+    }
+}

f05677552c9c4beab80a548631710e04c1f00b03
MakSharonov <mak.sharonov@gmail.com>
2016-05-12 01:03:53 +1000

    lesson 32

---
+++ p0321simplebrowser/src/main/java/com/vl/mak/simplebrowser/MainActivity.java
@@ -0,0 +1,26 @@
+package com.vl.mak.simplebrowser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    Button btnWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnWeb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://bash.im")));
+            }
+        });
+    }
+}

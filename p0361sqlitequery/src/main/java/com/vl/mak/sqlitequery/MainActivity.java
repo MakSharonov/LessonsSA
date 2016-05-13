ec8546119b75cb53b74bc635874318c7a9d7c765
mak.sharonov <mak.sharonov@gmail.com>
2016-05-13 17:53:30 +1000

    lesson 36 not finished

---
+++ p0361sqlitequery/src/main/java/com/vl/mak/sqlitequery/MainActivity.java
@@ -0,0 +1,132 @@
+package com.vl.mak.sqlitequery;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    final String LOG_TAG = "ma_log";
+
+    String name[] = { "Китай", "США", "Бразилия", "Россия", "Япония",
+            "Германия", "Египет", "Италия", "Франция", "Канада" };
+
+    int people[] = { 1400, 311, 195, 142, 128, 82, 80, 60, 66, 35 };
+
+    String region[] = { "Азия", "Америка", "Америка", "Европа", "Азия",
+            "Европа", "Африка", "Европа", "Европа", "Америка" };
+
+    Button btnAll, btnFunc, btnPeople, btnSort, btnGroup, btnHaving;
+    EditText etFunc, etPeople, etRegionPeople;
+    RadioGroup rgSort;
+
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAll = (Button) findViewById(R.id.btnAll);
+        btnAll.setOnClickListener(this);
+
+        btnFunc = (Button) findViewById(R.id.btnFunc);
+        btnFunc.setOnClickListener(this);
+
+        btnPeople = (Button) findViewById(R.id.btnPeople);
+        btnPeople.setOnClickListener(this);
+
+        btnSort = (Button) findViewById(R.id.btnSort);
+        btnSort.setOnClickListener(this);
+
+        btnGroup = (Button) findViewById(R.id.btnGroup);
+        btnGroup.setOnClickListener(this);
+
+        btnHaving = (Button) findViewById(R.id.btnHaving);
+        btnHaving.setOnClickListener(this);
+
+        etFunc = (EditText) findViewById(R.id.etFunc);
+        etPeople = (EditText) findViewById(R.id.etPeople);
+        etRegionPeople = (EditText) findViewById(R.id.etRegionPeople);
+
+        rgSort = (RadioGroup) findViewById(R.id.rgSort);
+
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        // проверка существования записей
+        Cursor c = db.query("mytable", null, null, null, null, null, null);
+        if (c.getCount() == 0) {
+            ContentValues cv = new ContentValues();
+            for (int i = 0; i < 10; i++) {
+                cv.put("name", name[i]);
+                cv.put("people", people[i]);
+                cv.put("region", region[i]);
+                Log.d(LOG_TAG, "id = " + db.insert("mytable", null, cv));
+            }
+        }
+        c.close();
+        dbHelper.close();
+
+        onClick(btnAll);
+    }
+
+    @Override
+    public void onClick(View v) {
+        db = dbHelper.getWritableDatabase();
+
+        String sFunc = etFunc.getText().toString();
+        String sPeople = etPeople.getText().toString();
+        String sRegionPeople = etRegionPeople.getText().toString();
+
+        String[] columns = null;
+        String selection = null;
+        String[] selectionArgs = null;
+        String groupBy = null;
+        String having = null;
+        String orderBy = null;
+
+        Cursor c = null;
+
+        switch (v.getId()) {
+            case R.id.btnAll:
+                Log.d(LOG_TAG, "--- Все записи: ---");
+                db.query("mytable", null, null, null, null, null, null);
+                break;
+        }
+
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            db.execSQL("create table mytable (" +
+                    "id integer primary key autoincrement," +
+                    "name text," +
+                    "people integer," +
+                    "region text" + ");");
+        }
+    }
+}
